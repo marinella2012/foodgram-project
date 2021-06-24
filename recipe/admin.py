1aea3dc44eaff576e27c029d394b2db5e23affa9
Marina <marinella2012@gmail.com>
2021-06-24 12:32:39 +0400

    some fix

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredient, Recipe, RecipeIngredient, Tag, Measurement
+from .models import Ingredient, Measurement, Recipe, RecipeIngredient, Tag
 
 
 @admin.register(Ingredient)
--- recipes/utils.py
+++ recipes/utils.py
@@ -8,21 +8,12 @@ from recipes.models import Ingredient, RecipeIngredient
 
 
 def get_ingredients(request):
-<<<<<<< HEAD:recipe/utils.py
-    ingredients = {}
-    for key, title in request.POST.items():
-        if key.startswith('nameIngredient'):
-            num = key.split('_')[1]
-            ingredients[title] = request.POST[f'valueIngredient_{num}']
-    return ingredients
-=======
     result = {}
     for key, value in request.POST.items():
         if key.startswith('nameIngredient'):
             num = key.split('_')[1]
             result[value] = request.POST[f'valueIngredient_{num}']
     return result
->>>>>>> new:recipes/utils.py
 
 
 def save_recipe(request, form, author=None, is_edit=False):
@@ -38,13 +29,8 @@ def save_recipe(request, form, author=None, is_edit=False):
         with transaction.atomic():
             ingredients = get_ingredients(request)
             recipe_ingredients = []
-<<<<<<< HEAD:recipe/utils.py
-            for title, quantity in ingredients.items():
-                ingredient = get_object_or_404(Ingredient, title=title)
-=======
             for name, quantity in ingredients.items():
                 ingredient = get_object_or_404(Ingredient, name=name)
->>>>>>> new:recipes/utils.py
                 obj = RecipeIngredient(
                     recipe=recipe,
                     ingredient=ingredient,
@@ -58,29 +44,3 @@ def save_recipe(request, form, author=None, is_edit=False):
         if is_edit:
             RecipeIngredient.objects.bulk_create(tmp_ingredients)
         return HttpResponseBadRequest
-<<<<<<< HEAD:recipe/utils.py
-
-
-def edit_recipe(request, form, instance):
-    try:
-        with transaction.atomic():
-            RecipeIngredient.objects.filter(recipe=instance).delete()
-            return save_recipe(request, form)
-    except IntegrityError:
-        raise
-
-
-def generate_pdf(template_name, context):
-    pdf_options = {
-        'page-size': 'Letter',
-        'margin-top': '0.75in',
-        'margin-right': '0.75in',
-        'margin-bottom': '0.75in',
-        'margin-left': '0.75in',
-        'encoding': "UTF-8",
-        'no-outline': None
-    }
-    html = get_template(template_name).render(context)
-    return pdfkit.from_string(html, False, options=pdf_options)
-=======
->>>>>>> new:recipes/utils.py
